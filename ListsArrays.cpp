3bff3b588613905b7d7ee546c31ff74556564ba8
Juan Casian <jp.capo.98@gmail.com>
2017-08-22 11:09:01 -0500

    Final program without comments

--- ListsArrays.cpp
+++ ListsArrays.cpp
@@ -5,12 +5,59 @@ Juan Casian
 */
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+double SumOfList (double ListOfNumbers[]){
+  double Total = 0;
+  for (int i = 0; i < 10; i++) {
+    Total = Total + ListOfNumbers[i];
+    //cout << Total << endl; This code was used to test how the addition of the terms was working
+  }
+  cout.precision(5);
+  cout << "The result of the addtions of the values in the list = " << Total << endl;
+  return Total;
+}
+
+double AverageOfList (double Total){
+  Total = Total / 10;
+  cout.precision(5);
+  cout << "The average of the values in the list is = " << Total << endl;
+  return Total;
+}
+
+void StandardDeviation (double ListOfNumbers [], double Average) {
+
+  double Sum;
+  double NumberElevated;
+  double SumOfAll = 0;
+  double StandardDeviation;
+
+  for (int i = 0; i < 10; i++){
+
+    Sum = (ListOfNumbers[i]-Average);
+    NumberElevated = pow(Sum, 2);
+    SumOfAll = SumOfAll + NumberElevated;
+
+    // cout << SumOfAll << endl;       Used to test the addition of all the diferences squared
+  }
+
+  double SumDividedByN;
+  SumDividedByN = SumOfAll / 10;
+
+  // cout << SumDividedByN << endl;     Another Test
+
+  StandardDeviation = sqrt (SumDividedByN);
+  cout.precision(4);
+  cout << "The Standard Deviation of the values in the list is = " << StandardDeviation << endl;
+}
 
 int main () {
 
 double ListOfNumbers [10];
+double Total = 0;
+double Average = 0;
 
 cout << "This is a Addition, Average and Standard Deviation for a list of numbers. Please enter 10 Values." << endl;
 cout << "Value #1: "; cin >> ListOfNumbers [0];
@@ -24,11 +71,20 @@ cout << "Value #8: "; cin >> ListOfNumbers [7];
 cout << "Value #9: "; cin >> ListOfNumbers [8];
 cout << "Value #10: "; cin >> ListOfNumbers [9];
 
-for (n = 0; n < 10; n++) {
-  cout << ListOfNumbers [n] << ' ' << endl;
-}
+/* for (int i = 0; i < 10; i++) {
+  cout << ListOfNumbers [i] << ' ' << endl;       This code is for printing each value of the array. It was useful to test the program.
+}         */
 
-}
+Total = SumOfList(ListOfNumbers);
 
+Average = AverageOfList(Total);
+
+StandardDeviation (ListOfNumbers, Average);
+
+cout << "Thank you for your time! SHUTTING DOWN..." << endl;
+
+
+
+return 0;
 
 }
